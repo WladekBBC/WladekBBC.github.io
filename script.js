d31b7f7da6ecbaabee505e6fd5043ff9cfacd2a3
WladekBBC <147035999+WladekBBC@users.noreply.github.com>
2025-03-18 22:35:58 +0100

    index HTML

---
+++ script.js
@@ -0,0 +1,21 @@
+let yes = document.querySelector('#yes')
+let survey = document.querySelector('.survey')
+
+const random = (min,max) => {
+    const rand = 10 + Math.random() * (max - min + 100);
+    return Math.floor(rand)
+}
+
+let no = document.querySelector('#no')
+no.addEventListener('mouseenter', () => {
+    no.style.left = `${random(100,200)}%`;
+    no.style.top = `${random(100,200)}%`;
+})
+
+no.addEventListener('click', function(){
+    survey.textContent = 'Shtop tryin';
+})
+
+yes.addEventListener('click', function(){
+    survey.textContent = 'Verry good choice)))';
+})
